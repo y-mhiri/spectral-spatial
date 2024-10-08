fcc5da86eb3551ed6a0254c2f6185b2c4b62c09a
y-mhiri <yassine.mhiri@outlook.fr>
2024-10-08 14:56:16 +0200

    corrected mu,lmbda loop

--- code/scripts/benchmark.py
+++ code/scripts/benchmark.py
@@ -123,79 +123,82 @@ params['loss_fn'] = {'sigma2': 1}
 
 residual_noise = torch.randn_like(dataset[0], device=device, dtype=dtype)
 
-for i, (lmbda, mu) in enumerate(zip(root.attrs['lambda_range'], root.attrs['mu_range'])):
-    print(f"Running trial {i} : lambda = {lmbda}, mu = {mu}")
-    print('-----------------------------------')  
-    group = root.create_group(f'trial_{i:02d}')
-    group.attrs['lambda'] = lmbda
-    group.attrs['mu'] = mu
+trial_idx = 0
+for lmbda in root.attrs['lambda_range']:
+    trial_idx += 1
+    for mu in root.attrs['mu_range']:
+        print(f"Running trial {trial_idx} : lambda = {lmbda}, mu = {mu}")
+        print('-----------------------------------')  
+        group = root.create_group(f'trial_{trial_idx:02d}')
+        group.attrs['lambda'] = lmbda
+        group.attrs['mu'] = mu
 
 
-    metrics = {}
-    reconstructed_ar = torch.zeros([len(subset), dataset.nband, dataset.height, dataset.width], device=device, dtype=dtype)
-    
-    loss_ar = torch.zeros([len(subset), max_iter], device=device, dtype=dtype)
-    for j,data in enumerate(subset):
-        print(f"Running data {j+1} on {len(subset)}")
-
-        # import image to device (cpu or gpu), sizes of x is [1,number of bands, width, height]
-        x = data.unsqueeze(0).to(device=device,dtype=dtype) 
-
-        # Adds a small amount of white gaussian noise (sigma^2 = 1e-4) to avoid numerical issues
-        x += 1e-2*residual_noise
-
-        # Compute the panchromatic image from the ground truth HSI
-        panc = torch.sum(x, dim=1).unsqueeze(1)/x.shape[1]
-        grad_panc = nabla(panc)
+        metrics = {}
+        reconstructed_ar = torch.zeros([len(subset), dataset.nband, dataset.height, dataset.width], device=device, dtype=dtype)
         
-        # Adds noise to the input HSI
-        sigma2 = 10**(-SNR/10) * torch.norm(x, dim=[2,3])**2 / x.shape[2] / x.shape[3]
-        sigma2 = sigma2.unsqueeze(0).unsqueeze(1).reshape(1, sigma2.numel(), 1, 1)
-        sigma2 = sigma2.repeat(1, 1, x.shape[2], x.shape[3])
+        loss_ar = torch.zeros([len(subset), max_iter], device=device, dtype=dtype)
+        for j,data in enumerate(subset):
+            print(f"Running data {j+1} on {len(subset)}")
 
-        y = x + torch.sqrt(sigma2)*torch.randn_like(x, device=device, dtype=dtype)
+            # import image to device (cpu or gpu), sizes of x is [1,number of bands, width, height]
+            x = data.unsqueeze(0).to(device=device,dtype=dtype) 
 
-        # Define optimization object
-        params['prox_tau_f'] = {'y': y.to(device=cuda,dtype=dtype), 'sigma2': 1}
-        optim = TVGradAlignement(max_iter=max_iter, 
-                                 mu=mu, 
-                                 lmbda=lmbda, 
-                                 theta=theta, 
-                                 sigma=sigma, 
-                                 tau=tau, 
-                                 grad_panc=grad_panc.to(device=cuda,dtype=dtype))
+            # Adds a small amount of white gaussian noise (sigma^2 = 1e-4) to avoid numerical issues
+            x += 1e-2*residual_noise
+
+            # Compute the panchromatic image from the ground truth HSI
+            panc = torch.sum(x, dim=1).unsqueeze(1)/x.shape[1]
+            grad_panc = nabla(panc)
+            
+            # Adds noise to the input HSI
+            sigma2 = 10**(-SNR/10) * torch.norm(x, dim=[2,3])**2 / x.shape[2] / x.shape[3]
+            sigma2 = sigma2.unsqueeze(0).unsqueeze(1).reshape(1, sigma2.numel(), 1, 1)
+            sigma2 = sigma2.repeat(1, 1, x.shape[2], x.shape[3])
+
+            y = x + torch.sqrt(sigma2)*torch.randn_like(x, device=device, dtype=dtype)
+
+            # Define optimization object
+            params['prox_tau_f'] = {'y': y.to(device=cuda,dtype=dtype), 'sigma2': 1}
+            optim = TVGradAlignement(max_iter=max_iter, 
+                                    mu=mu, 
+                                    lmbda=lmbda, 
+                                    theta=theta, 
+                                    sigma=sigma, 
+                                    tau=tau, 
+                                    grad_panc=grad_panc.to(device=cuda,dtype=dtype))
 
 
-        start_time = time.time()
+            start_time = time.time()
 
-        reconstructed, loss = optim(y.to(device=cuda,dtype=dtype), init=None, verbose=False, params=params)
+            reconstructed, loss = optim(y.to(device=cuda,dtype=dtype), init=None, verbose=False, params=params)
+            
+            compute_time = time.time() - start_time
+            group.attrs['time'] = compute_time
+            
+            reconstructed_ar[j] = reconstructed
+            loss_ar[j] = loss
+
+
+
+
+            sample_metrics = compute_metrics(gt=x.to(device=cuda,dtype=dtype), est=reconstructed, numpy=True)
+            for metric in sample_metrics:
+                if metric in metrics:
+                    metrics[metric].append(sample_metrics[metric])
+                else:
+                    metrics[metric] = [sample_metrics[metric]]
+
+            torch.cuda.empty_cache()
+
+
+
+        for metric in metrics:
+            group.attrs[metric] = metrics[metric]
+
+        group.create_dataset(f'reconstructed', data=reconstructed_ar.cpu().numpy())
+        group.create_dataset(f'loss', data=loss_ar.cpu().numpy())
         
-        compute_time = time.time() - start_time
-        group.attrs['time'] = compute_time
-        
-        reconstructed_ar[j] = reconstructed
-        loss_ar[j] = loss
 
-
-
-
-        sample_metrics = compute_metrics(gt=x.to(device=cuda,dtype=dtype), est=reconstructed, numpy=True)
-        for metric in sample_metrics:
-            if metric in metrics:
-                metrics[metric].append(sample_metrics[metric])
-            else:
-                metrics[metric] = [sample_metrics[metric]]
-
-        torch.cuda.empty_cache()
-
-
-
-    for metric in metrics:
-        group.attrs[metric] = metrics[metric]
-
-    group.create_dataset(f'reconstructed', data=reconstructed_ar.cpu().numpy())
-    group.create_dataset(f'loss', data=loss_ar.cpu().numpy())
-    
-
-    print(f"Finished lambda_tv_{lmbda}_mu_{mu}")
+        print(f"Finished lambda_tv_{lmbda}_mu_{mu}")
 
