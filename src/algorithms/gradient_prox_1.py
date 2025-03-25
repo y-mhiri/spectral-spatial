7f8a0b1eb0eb2c5884b79d73e8f664a693a77998
MAMOUR-data <ndiayemamour1997@gmail.com>
2025-03-25 10:38:14 +0100

    déclaration de la classe gradient proximale

---
+++ src/algorithms/gradient_prox_1.py
@@ -0,0 +1,99 @@
+import torch
+import torch.nn as nn
+from tqdm.auto import tqdm
+from nabla import nabla , nabla_adjoint
+from pansharpening import PANDataset
+
+class ProximalGradient(nn.Module):
+    """
+    Algorithme de gradient proximal pour résoudre le problème de pansharpening hyperspectral.
+
+    Attribut :
+    maxiter : nombre maximale d'itération
+    lambda : parametre de regularisation
+    lambda_m : regularisation sur l'attache aux données
+    tau : Pas de descend 
+    tol : critére de convergence
+
+    Methode :
+    Grad_f : calcul le gradient de l'attaches aux données
+    proj  : fait une projection de l'image si sa norme est supérieur à 1 
+    grad_proj : fait une descend de gradient projecté 
+    proxg : calcul l'operateur proximal de g 
+    forward : la fonction d'optimisation 
+    """
+
+    def __init__(self, max_iter=100, lmbda=1.0, lmbda_m=1.0, tau=0.1, tol=1e-7, verbose=True):
+        super(ProximalGradient, self).__init__()
+        self.max_iter = max_iter
+        self.lmbda = lmbda  # Paramètre de régularisation pour la variation totale
+        self.lmbda_m = lmbda_m  # Paramètre de régularisation pour l'attache aux données multispectrales
+        self.tau = tau  # Pas de gradient
+        self.tol = tol  # Tolérance pour la convergence
+        self.verbose = verbose  # Affichage des informations
+
+    
+    def grad_f(self, U, Y_H, Y_M,R):
+        """
+        Calcule le gradient de la fonction f(U).
+        """
+        # Terme 1 : Gradient de 1/2 ||Y_H -  U B||_F^2
+        #grad1 = (U @ B - Y_H) @ B.T # Si B est une matrice
+        # Si B est une fonction 
+        grad1 =  PANDataset.simule_low_hsi_adjoint((PANDataset.simule_low_hsi(U ,8) - Y_H),8)
+
+        # Terme 2 : Gradient de (lambda_m / 2) ||Y_M - R H U||_F^2
+        grad2 = self.lmbda_m * (R.T @ (R @ U - Y_M))
+
+        return grad1 + grad2
+    
+    def proj(self, z):
+        r"""
+        
+        projection sur la boule unité.
+
+
+        """
+
+        return z/ torch.maximum(torch.norm(z, dim=-1, keepdim=True), torch.ones_like(z))
+    
+
+    def grad_proj(self,x):
+        n, m,c = x.shape
+        z0 = np.ones((2, n, m,c))
+        for i in range(self.max_iter):
+            grad_z = -2 * nabla(nabla_adjoint(z0) + x / self.lamda)  
+            z = proj(z0 - self.tau * grad_z)
+            if convergence_criteria(z, z0, self.tol):  
+                break
+            z0 = z
+        return z
+    
+
+
+    
+    def proxg(self,x):
+        z = np.copy(x)
+        z = grad_proj(z, self.tau,self.max_iter,self.lmbda, self.tol)
+        y = x + self.lmbda * nabla_adjoint(z) 
+        return y
+    
+
+    def forward(self,U, Y_H, Y_M, R):
+        """
+        Résout le problème d'optimisation.
+        """
+        # Initialisation
+        h, w, _ = Y_M.shape
+        _, _, c = Y_M.shape
+        U = torch.zeros((h, w, c))
+
+        # Boucle d'optimisation
+        for it in tqdm(range(self.max_iter)):
+            # Gradient de f(U)
+            grad_U = self.grad_f(U, Y_H, Y_M, R)
+
+            # Mise à jour de U
+            U = self.proxg(U - self.lmbda * grad_U)
+
+        return U
