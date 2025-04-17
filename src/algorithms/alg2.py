ae7e062bd12daedaa90c3b64b31f0156def471ff
MAMOUR-data <ndiayemamour1997@gmail.com>
2025-04-17 15:09:52 +0200

    tv avec chambolle pock pour pans

--- src/algorithms/alg2.py
+++ src/algorithms/alg2.py
@@ -2,20 +2,38 @@ from gradient_prox import PANProximalGradient
 from tvprior import TVPrior
 
 class PANTVCB(PANProximalGradient):
-    """ 
-     
-    ....
-    
     """
-    def __init__(self, A, Aadj, R, max_iter, lmbda, lmbda_m, tau, tol, scale, verbose, **params):
-        super().__init__(A, Aadj, R, max_iter, lmbda, lmbda_m, tau, tol, scale, verbose)
+    Calcul de l'opérateur proximale de la TV vectorielle en utilisant chamboll pock.
+    Attributs:
+        solver (class): résoud l'algo de chamboll pock avec les paramétre nécessaire.
 
-        self.solver = TVPrior(**params)
+    Methods:
+    proxg(input_image)
+       cette fonction donne l'opérateur proximale de l'image d'entrée en faisant un algo de chamboll pock
+
+    """
+    def __init__(self, A,Aadj, max_iter, lmbda, lmbda_m, tol, scale, verbose, params):
+        super().__init__(A, Aadj, max_iter, lmbda, lmbda_m, tol, scale, verbose)
+
+        self.optim = TVPrior(**params)
 
 
     def proxg(self,x):
         """
-        ....
+        Donne l'opérateur proximale de la tv vectorielle avec chamboll pock ....
         
         """
-        return self.solver(x)
+        params = {}
+
+        params['compute_L'] = {'nband': 31}
+        params['K'] = {}
+        params['K_adjoint'] = {}
+        params['prox_sigma_g_conj'] = {}
+        params['prox_tau_f'] = {'y': x, 'sigma2': 1}
+        params['loss_fn'] = {}
+
+
+        return self.optim(x,init=None, verbose=False, params=params, return_loss=False)
+    
+
+   
