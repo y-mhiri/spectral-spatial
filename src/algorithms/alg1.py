55550e022e7a813dae681656b5433a80df1461de
MAMOUR-data <ndiayemamour1997@gmail.com>
2025-04-17 15:07:55 +0200

    gradient projete pour pans

--- src/algorithms/alg1.py
+++ src/algorithms/alg1.py
@@ -1,57 +1,89 @@
-
 from gradient_prox import PANProximalGradient
 import torch
-
 from nabla import nabla, nabla_adjoint
+import numpy as np
+
 
 class PANTVGradProj(PANProximalGradient):
     """
+    Calcul de l'opérateur proximal de la TV vectorielle par descente de gradient projeté.
     
-    ....
-    
+    Attributs:
+        max_iter_gp (int): Nombre maximal d'itérations pour la descente de gradient
+        tau (float): Pas de descente
     """
-    def __init__(self, A, Aadj, R, max_iter, lmbda, lmbda_m, tau, tol, scale, verbose, max_iter_gp):
-        super().__init__(A, Aadj, R, max_iter, lmbda, lmbda_m, tau, tol, scale, verbose)
 
+    def __init__(self, A, Aadj, max_iter, lmbda, lmbda_m, tau, tol, scale, verbose, max_iter_gp):
+        """
+        Initialise les paramètres de l'algorithme.
+        
+        Args:
+            max_iter_gp (int): Nombre max d'itérations pour la sous-optimisation
+            tau (float): Pas de descente pour le gradient projeté
+        """
+        super().__init__(A, Aadj,max_iter, lmbda, lmbda_m, tol, scale, verbose)
         self.max_iter_gp = max_iter_gp
+        self.tau = tau
 
+    def proj(self, z):
+        """
+        Projection sur la boule unité.
+        
+        Args:
+            z (torch.Tensor): Tenseur [b,c,h,w,2]
+            
+        Returns:
+            torch.Tensor: Tenseur projeté [b,c,h,w,2]
+        """
+        #norm_z = torch.sqrt(torch.sum(z**2, dim=-1, keepdim=True))
+        return z / torch.maximum(torch.norm(z, dim=-1, keepdim=True), torch.ones_like(z))
+    
 
     def grad_proj(self, x):
         """
-        Descente de gradient projeté.
+        Descente de gradient projeté avec accélération FISTA pour résoudre le sous-problème TV.
         
         Args:
             x (torch.Tensor): Tenseur [b,c,h,w]
             
         Returns:
-            torch.Tensor: Minimum [b,c,h,w]
-        """
+            torch.Tensor: Solution [b,c,h,w,2]
+    """
         b, c, h, w = x.shape
-        z0 = torch.ones((b, c, h, w, 2))
-        
-        for i in range(self.max_iter):
-            grad_z = -2 * nabla(nabla_adjoint(z0) + x / self.lmbda)
-            z = self.proj(z0 - self.tau * grad_z)
-            
-            if self.convergence_criteria(z, z0):
-                break
-                
-            z0 = z
-            
-        return z
+        w = torch.ones((b, c, h, w, 2), device=x.device, dtype=x.dtype)
+        y = w.clone()
+        t = 1.0
     
+        # FISTA acceleration
+        for i in range(self.max_iter_gp):
+            w_prev = w.clone()
+            
+            # Calcul du gradient
+            grad_z = 2 * nabla(nabla_adjoint(y) + x / self.lmbda)
+            
+            # Mise à jour avec projection
+            w = self.proj(y - self.tau * grad_z)
+            
+            # Mise à jour de l'accélération FISTA
+            t_prev = t
+            t = (1 + np.sqrt(1 + 4 * t**2)) / 2
+            y = w + ((t_prev - 1) / t) * (w - w_prev)
+            
+            # Critère de convergence
+            if self.convergence_criteria(w, w_prev):
+                break
+            
+        return w
 
     def proxg(self, x):
         """
-        Opérateur proximal de x.
+        Opérateur proximal avec régularisation TV.
         
         Args:
-            x (torch.Tensor): Tenseur [b,c,h,w]
+            x (torch.Tensor): Image d'entrée [b,c,h,w]
             
         Returns:
-            torch.Tensor: Image projetée [b,c,h,w]
+            torch.Tensor: Image régularisée [b,c,h,w]
         """
-        z = torch.clone(x)
-        z = self.grad_proj(z)
+        z = self.grad_proj(x)
         return x + self.lmbda * nabla_adjoint(z)
-    
