b55e3a553df1f2e514b7521b6f21ecc7e4e77d3d
MAMOUR-data <ndiayemamour1997@gmail.com>
2025-04-29 13:09:52 +0200

    sauvegarde des modif

--- src/tests/test_gradient_prox.py
+++ src/tests/test_gradient_prox.py
@@ -7,8 +7,9 @@ import torch
 
 import matplotlib.pyplot as plt
 
-from pansharpening import PANDataset
-from gradient_prox_1 import ProximalGradient
+from pans import PANDatasetImproved
+from gradient_prox import PANProximalGradient
+from alg1 import PANTVGradProj
 from torchvision import transforms
 from math import sqrt
 
@@ -27,31 +28,36 @@ if __name__ == '__main__':
 
     # Define data path
     data_path = '/home/ndiayem/Documents/spectral-spatial/data/harvard.zarr'
+    crop_size = 128
 
-
-    val_transform = transforms.Compose([transforms.ToTensor()]) # Transforms a the input data to torch tensors
-    dataset = PANDataset(root_dir=data_path, split='train', transform= val_transform)
-
-
+    val_transform  =  transforms.Compose([ transforms.CenterCrop(crop_size)])
+    dataset =PANDatasetImproved(root_dir=data_path, split='train' ,transform=val_transform,normalize=True,scale=2,sigma=1.5,device=device,size =crop_size)
     idx = 17
     X = dataset[idx]
     X = X.unsqueeze(0)
 
     # Matrice de transformation
     Y_H = dataset.simule_low_hsi(X)  
-    Y_M = dataset.get_panchromatic(X)
-    Y_M = Y_M                                                             # Transposée de B 
+    Y_M = dataset.get_panchromatic(X)                                                            # Transposée de B 
     
 
-    params = {
-        'max_iter_cb' : 1000
-        'theta_cb' : 1
-        ...
-    }
+    solver_gp = PANTVGradProj(
+    A=dataset.simule_low_hsi,
+    Aadj=dataset.simule_low_hsi_adjoint,
+    up = dataset.S_up,
+    R=dataset.spectral,
+    max_iter=900,
+    lmbda=1e-2,
+    lmbda_m=2,
+    tau=0.08,
+    tol=1e-7,
+    scale=4,
+    verbose=True,
+    max_iter_gp=100
+    )
     
-    pansharpening_model = ProximalGradient(dataset.simule_low_hsi,dataset.simule_low_hsi_adjoint ,max_iter=100, lmbda=0.1, lmbda_m=1, tau=0.1,tol=1e-7 ,scale = 8,verbose=True, **param_cb)
-
+    
     # Exécution de l'optimisation
-    U_result = pansharpening_model(Y_H, Y_M)
+    U_gp, costs_gp = solver_gp(Y_H, Y_M)
 
     print('OK.')
