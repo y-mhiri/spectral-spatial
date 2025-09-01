a69f299f505b94544d30eb3422ec0946fa3ab8ef
Yassine MHIRI <mhiriy@lappusmb9c.in2p3.fr>
2025-09-01 10:08:48 +0200

    Automatic commit before running experiment PANCTV: debug

--- experiments/PANCTV/experiment_ctv.py
+++ experiments/PANCTV/experiment_ctv.py
@@ -1,7 +1,9 @@
 import sys
 import os 
 
-path = os.path.join(os.getenv("HOME"), 'spectral-spatial/src')
+#
+HOME = "/uds_data/listic/mhiriy"
+path = os.path.join(HOME, 'spectral-spatial/src')
 sys.path.append(os.path.join(path, 'datasets'))
 sys.path.append(os.path.join(path, 'algorithms'))
 sys.path.append(os.path.join(path, 'metrics'))
