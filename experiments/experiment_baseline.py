8baa599ca253a650383b62472739455bfa0f81cd
y-mhiri <yassine.mhiri@outlook.fr>
2025-10-03 12:57:19 +0200

    Automatic commit before running experiment lambda_tuning: debug

--- experiments/experiment_baseline.py
+++ experiments/experiment_baseline.py
@@ -130,7 +130,7 @@ def main():
         total_time += compute_time
         
         # Store results
-        reconstructed = torch.clamp(min=0, max=1)
+        reconstructed = torch.clamp(reconstructed, min=0.0, max=1.0)
         reconstructed_ar[j] = reconstructed
         loss_ar[j] = loss
         relval_ar[j] = relval
